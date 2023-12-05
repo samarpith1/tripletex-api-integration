a97bd16bc3334391fb6fa93cd9fecb974f478eed
AnandhuRajagopal <anandhu.nair@pragtech.co.in>
2023-12-05 15:26:38 +0530

    updated import contact function

--- pragtech_odoo_tripletex_connector/models/__init__.py
+++ pragtech_odoo_tripletex_connector/models/__init__.py
@@ -2,5 +2,4 @@
 
 from . import res_company_inherit
 from . import triplex_account_inherit
-# from . import tripletex_credential
 from . import session_token
--- pragtech_odoo_tripletex_connector/models/res_company_inherit.py
+++ pragtech_odoo_tripletex_connector/models/res_company_inherit.py
@@ -3,6 +3,7 @@ from odoo.exceptions import UserError
 import requests
 import datetime
 from dateutil.parser import parse
+import base64
 
 class ResCompany(models.Model):
 
@@ -13,4 +14,48 @@ class ResCompany(models.Model):
     consumer_token = fields.Char()
     employee_token = fields.Char()
     token =  fields.Char(string='Token',)
+
+
+
+    def tripletex_import_contacts(self):
+        url = 'https://api.tripletex.io/v2/contact'
+        consumer_token = self.consumer_token  
+        employee_token = self.employee_token
+        company_id = '12841878'
+        session_token = self.token
+
+        auth_token = f'{company_id}:{session_token}'
+        encoded_token = base64.b64encode(auth_token.encode('utf-8')).decode('utf-8')
+        print("::::::::::::::::::::::::::::::::::::::::::::::ENCODED TOKEN:::::::::::::::::::::::::::::::::::::::::::",encoded_token)
+
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Basic {encoded_token}',
+            'consumerToken': consumer_token,
+            'employeeToken': employee_token
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            print("::::::::::::::::::::::::::::::::::::RESPONSE::::::::::::::::::::::::::::::::::::::::",response)
+            token = self.env['res.partner'].search([])
+            print("::::::::::::::::::::::::::::::::::::TOKEN SEARCH:::::::::::::::::::::::::::::::::::::::::::",token)
+            if response.status_code == 200:
+                json_data = response.json()
+                print("::::::::::::::::::::::::::::::::::::::JSON::::::::::::::::::::::::::::::::::::::::::::::::",json_data)
+                if token:
+                    print("::::::::::::::::::::::::::::::::::::::SUCCESS TOKEN IF::::::::::::::::::::::::::::::::::::::::::::::::")
+                    token.create({
+                            'mobile': json_data['values']['phoneNumberMobile']
+                        })
+                else:
+                    print("::::::::::::::::::::::::::::::::::::::SUCCESS TOKEN IF-ELSE::::::::::::::::::::::::::::::::::::::::::::::::")
+                    print(f"Error: {response.status_code}, {response.text}")
+            else:
+                print("::::::::::::::::::::::::::::::::::::::RESPONSE ELSE::::::::::::::::::::::::::::::::::::::::::::::::")
+                pass
+
+        except Exception as e:
+            print(f"Error during request: {e}")
+
     
