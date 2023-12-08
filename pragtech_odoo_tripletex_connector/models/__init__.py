dc1a4e43fa3c5641b26542c791db2632b287c232
samarpith1 <arpith.samuel@pragtech.co.in>
2023-12-08 10:43:04 +0530

    [export of contact]

--- pragtech_odoo_tripletex_connector/models/__init__.py
+++ pragtech_odoo_tripletex_connector/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company_inherit
 from . import triplex_account_inherit
 from . import session_token
 from . import res_partner_inherit
+from . import res_company_inherit
+
--- pragtech_odoo_tripletex_connector/models/res_company_inherit.py
+++ pragtech_odoo_tripletex_connector/models/res_company_inherit.py
@@ -4,6 +4,8 @@ import requests
 import datetime
 from dateutil.parser import parse
 import base64
+import logging
+_logger = logging.getLogger(__name__)
 
 class ResCompany(models.Model):
 
@@ -14,7 +16,6 @@ class ResCompany(models.Model):
     consumer_token = fields.Char()
     employee_token = fields.Char()
     token =  fields.Char(string='Token',)
-
     
     
     
@@ -81,68 +82,67 @@ class ResCompany(models.Model):
                 
 
 
-
     
+    
+    
+    
+ 
 
-    # @api.model
-    # def create_customer_in_tripletex(self):
-    #     # Assuming 'xero_oauth_token' is the Tripletex API token in your res.partner model
-    #     partners = self.env['res.partner'].search([])
+ 
 
-    #     for partner in partners:
-    #         self.create_main_customer_in_tripletex(partner)
-        
-    #     success_form = self.env.ref('your_module.export_successfull_view', False)
-    #     return {
-    #         'name': _('Notification'),
-    #         'type': 'ir.actions.act_window',
-    #         'view_type': 'form',
-    #         'view_mode': 'form',
-    #         'res_model': 'res.company.message',
-    #         'views': [(success_form.id, 'form')],
-    #         'view_id': success_form.id,
-    #         'target': 'new',
-    #     }
+    def export_contacts_to_tripletex(self):
+        try:
+            
 
-    # def create_main_customer_in_tripletex(self, partner):
-    #     url = 'https://tripletex.no/v2/contact'
-    #     tripletex_token = partner.tripletex_api_token  # Replace with the field in res.partner containing Tripletex API token
+            partners = self.env['res.partner'].search([])
 
-    #     headers = {
-    #         'Accept': 'application/json',
-    #         'Authorization': f'Tripletex {tripletex_token}',
-    #         'Content-Type': 'application/json; charset=utf-8',
-    #     }
+            for partner in partners:
+            
+       
+                url = 'https://api.tripletex.io/v2/contact'
+                consumer_token = self.consumer_token 
+                employee_token = self.employee_token  
+                company_id = '12841878' 
+                session_token = self.token  
 
-    #     # Create the payload based on your Tripletex contact structure
-    #     payload = {
-    #         "id": 0,
-    #         "version": 0,
-    #         "firstName": partner.first_name,
-    #         "lastName": partner.last_name,
-    #         # Add other fields as needed
-    #     }
+            
+                auth_token = f'{company_id}:{session_token}'
+                encoded_token = base64.b64encode(auth_token.encode('utf-8')).decode('utf-8')
 
-    #     try:
-    #         response = requests.post(url, headers=headers, json=payload)
+                headers = {
+                    'Accept': 'application/json',
+                    'Authorization': f'Basic {encoded_token}',
+                    'consumerToken': consumer_token,
+                    'employeeToken': employee_token
+                }
+                
+                payload = {
+                    "id": 0,
+                    "version": 0,
+                    "firstName": partner.name,
+                    "email": partner.email,
+                    
+                }
+                print("::::::::::::::::::::payload::::::::::", payload)
+                response = requests.post(url, headers=headers, json=payload)
+                print("---------------response---------", response)
 
-    #         if response.status_code == 200:
-    #             # Request was successful
-    #             json_data = response.json()
-    #             self.process_tripletex_response(json_data)  # Implement this method to handle the response
+                if response.status_code == 200:
+                    json_data = response.json()
+                    self.process_tripletex_response(json_data)
+                else:
+                    print(f": {response.status_code}, {response.text}")
 
-    #         else:
-    #             print(f"Error: {response.status_code}, {response.text}")
+        except Exception as e:
+            print(f"Error during request: {e}")
 
-    #     except Exception as e:
-    #         print(f"Error during request: {e}")
-
-    # def process_tripletex_response(self, response):
-    #     # Implement logic to handle the Tripletex API response
-    #     pass
-
-    def tripletex_export_contacts(self):
-        print("-----work this print")
+            
+            
+            
+            
+            
+            
+          
 
 
 
@@ -200,3 +200,36 @@ class ResCompany(models.Model):
             self.env['product.template'].create(product_data)
             print("::::::::::::::::::::::::::::::::::::::::::::PARTNER_DATA_CREATE::::::::::::::::::::::::::::::::::::::::::::::::::",product_data)
 
+    
+    def tripletex_import_taxes(self):
+        url = 'https://api.tripletex.io/v2/contact'
+        consumer_token = self.consumer_token 
+        employee_token = self.employee_token  
+        company_id = '12841878' 
+        session_token = self.token  
+
+       
+        auth_token = f'{company_id}:{session_token}'
+        encoded_token = base64.b64encode(auth_token.encode('utf-8')).decode('utf-8')
+
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Basic {encoded_token}',
+            'consumerToken': consumer_token,
+            'employeeToken': employee_token
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                
+                json_data = response.json()
+                self.process_tripletex_contacts(json_data['values'])
+
+            else:
+                print(f"Error: {response.status_code}, {response.text}")
+
+        except Exception as e:
+            print(f"Error during request: {e}")
+            
